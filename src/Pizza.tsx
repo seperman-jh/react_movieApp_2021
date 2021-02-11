ae44c151637036145e1251abf788009262c98d7f
seperman <seperman.jh@gmail.com>
2021-02-11 01:44:23 -0800

    Developer : react_movieapp_2021_first #3

---
+++ src/Pizza.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function Pizza(){
+    return(
+
+      <h2>Pizza - Topping</h2>
+
+    );
+
+
+}
+
+export default Pizza;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Pizza from './Pizza';
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
+
+
+ReactDOM.render(
+    <Pizza />,
+    document.getElementById('content')
+);
+
 
