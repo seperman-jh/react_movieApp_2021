e227189ec9fbc5aae309e64ece7ebc7087213957
jhlee <jhlee@koreatimesus.com>
2021-02-14 00:55:59 -0800

    Developer :react_movieapp_2021_first # setState, state refresh, reRender, Current, Interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+interface IState {
+    count: number;
+}
 
-class App extends React.Component {
+class App extends React.Component <{}, IState>  {
+
+    state = {
+      count : 0
+    };
+
+    add = () => {
+
+       this.setState(current => ({count: current.count +1}));
+
+    };
+
     render (){
         return (
-            <h2>
-                hi111
-            </h2>
+            <div className = "State_Class">
+                <h2>
+                    hi111
+                </h2>
+                <h3>
+                    count : {this.state.count}
+                </h3>
+
+                <button onClick={() => this.add()}>add</button>
+                <button>minus</button>
+
+            </div>
         );
-    }
+    };
+
+
+
+
 }
 
 export default App;
